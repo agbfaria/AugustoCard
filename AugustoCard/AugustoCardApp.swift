9973d70f3b19f6af80a5e56a1c7da7ee632fc1a6
Augusto Faria <agbfaria@gmail.com>
2022-01-30 14:56:03 -0500

    Initial Commit

---
+++ AugustoCard/AugustoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AugustoCardApp.swift
+//  AugustoCard
+//
+//  Created by Augusto on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AugustoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
